408f4110959dea8edb559b3e18b7983b7a00209b
HEMANTH K <104354484+HEMANTH-1508@users.noreply.github.com>
2024-07-07 12:27:49 +0530

    Create compareto.java

---
+++ DAY 2 THEORY/compareto.java
@@ -0,0 +1,16 @@
+public class Example
+{
+    public static void main(String arg[])
+    {
+        String s="Whatever it may be, just do it as said by the higher authority......";
+        String t="You cant do whatever you like to do in the class without any acknowledgement..";
+        int a=s.compareTo(t);
+        System.out.println(a);
+
+    }
+}
+
+/*
+OUTPUT------
+  -2
+  */
