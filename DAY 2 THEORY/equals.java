b322bfa31221b1985fa6362f36df164949912ff6
HEMANTH K <104354484+HEMANTH-1508@users.noreply.github.com>
2024-07-07 12:36:39 +0530

    Create equals.java

---
+++ DAY 2 THEORY/equals.java
@@ -0,0 +1,16 @@
+public class Example
+{
+    public static void main(String arg[])
+    {
+        String s="Whatever it may be, just do it as said by the higher authority......";
+        String t="You cant do whatever you like to do in the class without any acknowledgement..";
+        boolean a=s.equals(t);
+        System.out.println(a);
+
+    }
+}
+
+/*
+OUTPUT---
+  false
+  */
