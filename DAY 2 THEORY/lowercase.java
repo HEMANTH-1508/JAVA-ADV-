dd68bc211d3fcdeec3ba3f080e82d945fdb4502b
HEMANTH K <104354484+HEMANTH-1508@users.noreply.github.com>
2024-07-07 12:21:50 +0530

    Create lowercase.java

---
+++ DAY 2 THEORY/lowercase.java
@@ -0,0 +1,17 @@
+public class Example
+{
+    public static void main(String arg[])
+    {
+        String s="Whatever it may be, just do it as said by the higher authority......";
+        //String t="You cant do whatever you like to do in the class without any acknowledgement..";
+        String a=s.toLowerCase();
+        System.out.println(a);
+
+    }
+}
+
+
+/*
+OUTPUT-----
+  whatever it may be, just do it as said by the higher authority......
+  */
