7b0cd87a093b904bc4da2b6ec36c197e3fd3025d
HEMANTH K <104354484+HEMANTH-1508@users.noreply.github.com>
2024-07-07 12:18:20 +0530

    Create Concat.java

---
+++ DAY 2 THEORY/Concat.java
@@ -0,0 +1,18 @@
+public class Example
+{
+    public static void main(String arg[])
+    {
+        String s="Whatever it may be, just do it as said by the higher authority......";
+        String t="You cant do whatever you like to do in the class without any acknowledgement..";
+        String a=s.concat(t);
+        System.out.println(a);
+
+    }
+}
+
+
+/*
+OUTPUT----
+Whatever it may be, just do it as said by the higher authority......You cant do whatever you like to do in the class without any acknowledgement..
+
+*/
