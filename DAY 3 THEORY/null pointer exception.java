b06f2750ba166341153012292adfb4cddcf0f8fd
HEMANTH K <104354484+HEMANTH-1508@users.noreply.github.com>
2024-07-07 13:19:52 +0530

    Create null pointer exception.java

---
+++ DAY 3 THEORY/null pointer exception.java
@@ -0,0 +1,15 @@
+public class Arr 
+{
+    public static void main(String[] args) 
+  {
+        try 
+          {
+            String s = new String();
+            System.out.println(s);
+        } 
+        catch (Exception e)  
+        {
+            System.out.println(e);
+        }
+    }
+}
