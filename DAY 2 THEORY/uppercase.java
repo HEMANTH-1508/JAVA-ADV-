a580b63468fd2835bd59208a3719f88b7995e1f3
HEMANTH K <104354484+HEMANTH-1508@users.noreply.github.com>
2024-07-07 12:25:03 +0530

    Create uppercase.java

---
+++ DAY 2 THEORY/uppercase.java
@@ -0,0 +1,16 @@
+public class Example
+{
+    public static void main(String arg[])
+    {
+        String s="Whatever it may be, just do it as said by the higher authority......";
+        //String t="You cant do whatever you like to do in the class without any acknowledgement..";
+        String a=s.toUpperCase();
+        System.out.println(a);
+
+    }
+}
+
+/*
+OUTPUT----
+  WHATEVER IT MAY BE, JUST DO IT AS SAID BY THE HIGHER AUTHORITY......
+  */
